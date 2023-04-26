e900a8c536d256b6d90d0e376bdd13129e1fa57d
Rihard4a <118355184+Rihard4a@users.noreply.github.com>
2023-04-26 13:01:26 +0300

    started learning react

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.scss";
+
+const name = "Rihards";
+const position = "Software Developer";
+const skills = [
+  "C#",
+  ".NET",
+  "TypeScript",
+  "JavaScript",
+  "React",
+  "Node.js",
+  "Git",
+  "HTML",
+  "CSS",
+  "SASS",
+];
+const hobbies = {
+  Gaming: "CS:GO,  League of Legends",
+  Poker: "Texas Holdem",
+  Weightlifting: "Strength Training",
+} as any;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 className="nameHeader">{name}</h2>
+      <h3 className="positionHeader">{position}</h3>
+      <h3 className="skillsHeader">Skills</h3>
+      <ul className="skillsList">
+        {skills.map((skill, index) => {
+          return <li key={index}>{skill}</li>;
+        })}
+      </ul>
+      <h3 className="hobbiesHeader">Hobbies</h3>
+      <ul className="hobbiesList">
+        {Object.keys(hobbies).map((hobby, index) => {
+          return (
+            <li key={index}>
+              {hobby}: {hobbies[hobby]}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }

c7ae02b626b68ac9dddb85d7b03b24cedfaed307
Rihard4a <118355184+Rihard4a@users.noreply.github.com>
2023-04-29 09:45:01 +0300

    refactored some code

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./styles/App.scss";
 import { TodoItemIsDone } from "./components/TodoItemIsDone";
 import { TodoItemDelete } from "./components/TodoItemDelete";
 import { TodoItemEdit } from "./components/TodoItemEdit";
+import { TodoForm } from "./components/TodoForm";
 
 export type Todo = {
   title: string;
@@ -11,15 +12,8 @@ export type Todo = {
   description?: string;
 };
 
-// Mājas darbs:
-// Button componenete, kas izdzēš todo item.
-// Iznest formu un todo list uz jaunu komponenti!
-// Uztaisīt edit opciju, kad parādās input laukus un var updeitot todo item title
-// Nostilot visu.
-
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [inputValue, setInputValue] = useState("");
 
   const handleDelete = (id: string) => {
     const newTodos = todos.filter((todo) => todo.id !== id);
@@ -40,57 +34,38 @@ function App() {
     setTodos(newTodos);
   };
 
+  const handleCheckboxChange = (id: string) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          isDone: !todo.isDone,
+        };
+      }
+      return todo;
+    });
+
+    setTodos(newTodos);
+  };
+
+  const handleAddTodo = (todo: Todo) => {
+    setTodos([...todos, todo]);
+  };
+
   return (
     <div className="container">
       <h1>Todo App</h1>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          const newTodo: Todo = {
-            title: inputValue,
-            id: Math.random().toString(),
-            isDone: false,
-          };
-          setTodos([...todos, newTodo]);
-          setInputValue("");
-        }}
-      >
-        <label htmlFor="todo-title">Add todo</label>
-        <input
-          id="todo-title"
-          type="text"
-          value={inputValue}
-          placeholder="Izglābt pasauli..."
-          onChange={(event) => {
-            const newValue = event.target.value;
-            setInputValue(newValue);
-          }}
-        />
-
-        <button type="submit">Add</button>
-      </form>
-
+      <TodoForm onSubmit={handleAddTodo} />
       <ul>
         {todos.map((todo) => {
           return (
             <li key={todo.id}>
-            <TodoItemIsDone
-              todo={todo}
-              key={todo.id}
-              onCheckboxChange={() => {
-                const newTodos = todos.map((todoItem) => {
-                  if (todoItem.id === todo.id) {
-                    return {
-                      ...todoItem,
-                      isDone: !todoItem.isDone,
-                    };
-                  }
-                  return todoItem;
-                });
-
-                setTodos(newTodos);
-              }}
-            />
+              <TodoItemIsDone
+                todo={todo}
+                onCheckboxChange={() => {
+                  handleCheckboxChange(todo.id);
+                }}
+              />
               <TodoItemDelete todo={todo} onDelete={handleDelete} />
               <TodoItemEdit todo={todo} onEdit={handleEdit} />
             </li>
---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,38 @@
+import { Todo } from "../App";
+import { useState } from "react";
+
+type TodoFormProps = {
+  onSubmit: (newTodo: Todo) => void;
+};
+
+export const TodoForm = ({ onSubmit }: TodoFormProps) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newTodo: Todo = {
+      title: inputValue,
+      id: Math.random().toString(),
+      isDone: false,
+    };
+    onSubmit(newTodo);
+    setInputValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="todo-title">Add todo</label>
+      <input
+        id="todo-title"
+        type="text"
+        value={inputValue}
+        placeholder="Izglābt pasauli..."
+        onChange={(event) => {
+          const newValue = event.target.value;
+          setInputValue(newValue);
+        }}
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
